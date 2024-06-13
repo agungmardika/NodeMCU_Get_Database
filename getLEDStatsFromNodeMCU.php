9bd3dd86f8051c6afef9c4dc3a0117bd747721c0
agungmardika <agungdmardika@gmail.com>
2024-06-13 20:46:00 +0800

    web commit

---
+++ getLEDStatsFromNodeMCU.php
@@ -0,0 +1,5 @@
+<?php
+$getLEDStatusFromNodeMCU = $_POST["getLEDStatusFromNodeMCU"];
+$Write = "<?php $" . "getLEDStatusFromNodeMCU='" . $getLEDStatusFromNodeMCU . "'; " . "echo $" . "getLEDStatusFromNodeMCU;" . " ?>";
+echo $getLEDStatusFromNodeMCU;
+file_put_contents('LEDStatContainer.php', $Write);
